1526974cd4cd9266972cb37356cba88581bb58bd
stockslager <149531737+stockslager@users.noreply.github.com>
2026-05-26 20:09:48 -0400

    Update ConfigManager.js

--- hcc/core/js/ConfigManager.js
+++ hcc/core/js/ConfigManager.js
@@ -77,6 +77,8 @@ class ConfigurationItem {
         this.usePlantProjectImages = configData.use_plant_project_images ?? null;
         this.mapType         = configData.map_type ?? null;
         this.defaultMapZoom  = configData.default_map_zoom ?? null;
+        this.mapCenter       = configData.map_center ?? null;
+        this.mapExtent       = configData.map_extent ?? null;
         this.defaultPlace = configData.default_place ?? null;
         this.fieldId = configData.field_id ?? null;
         this.fieldName = configData.field_name ?? null;
